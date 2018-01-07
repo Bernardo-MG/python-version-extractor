6934eb847d1c61e6d424ab7e7daae253489d2a66
Bernardo <programming@wandrell.com>
2018-01-07 20:23:42 +0100

    Changed test resources paths for running the tests with tox

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -29,7 +29,7 @@ class TestExtractVersion(unittest.TestCase):
 
         This is the simples use case.
         """
-        version = extract_version('resources/__init__.py')
+        version = extract_version('tests/resources/__init__.py')
         self.assertEqual('1.2.3', version)
 
     def test_empty_file(self):
@@ -38,5 +38,5 @@ class TestExtractVersion(unittest.TestCase):
 
         This is the simples use case.
         """
-        version = extract_version('resources/empty.py')
+        version = extract_version('tests/resources/empty.py')
         self.assertFalse(version)
